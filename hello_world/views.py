50469aa506c8af40e0f7092a34946913e4981b60
ichha <thapa.ichha@gmail.com>
2025-11-03 05:22:13 +0545

    Update views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,17 +1,4 @@
-# hello_world/views.py
 from django.views.generic import TemplateView
-from django.template.response import TemplateResponse
 
 class HelloView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        html = """
-        {% extends "base.html" %}
-        {% block content %}
-            <div class="container mt-5">
-                <h1 class="text-success">Inline Success! 🎉</h1>
-                <p>View is routing. Templates next.</p>
-                <p>URL: {{ request.path }}</p>
-            </div>
-        {% endblock %}
-        """
-        return TemplateResponse(request, html)
+    template_name = "hello_world/hello.html"
