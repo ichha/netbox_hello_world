83af61376c4ab03c37adce94bba425c68be5791b
ichha <thapa.ichha@gmail.com>
2025-11-03 05:33:32 +0545

    Update views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,4 +1,9 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HelloView(TemplateView):
-    template_name = "hello_world/hello.html"
+    def get(self, request):
+        return render(request, 'hello_world/hello.html', {
+            'title': 'My Simple Plugin Page',
+            'message': 'Hello from the NetBox plugin!'
+        })
