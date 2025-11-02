f257a14709015ef4d23ff792d7808e40e9d8e23d
ichha <thapa.ichha@gmail.com>
2025-11-02 15:31:37 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="Hello‑World plugin for NetBox 4.4.4",
     packages=find_packages(),
-    install_requires=["netbox==4.4.4"],
     include_package_data=True,
     zip_safe=False,
 )
+
