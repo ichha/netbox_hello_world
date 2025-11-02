b9affface9ce5fb1f170d0a186de6148119f8bbf
ichha <thapa.ichha@gmail.com>
2025-11-02 15:20:05 +0545

    Update urls.py

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "hello_world"
-
 urlpatterns = [
-    path("", views.HelloView.as_view(), name="hello"),
+    path('', views.HelloView.as_view(), name='hello'),
 ]
