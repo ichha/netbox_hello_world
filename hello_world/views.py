c16c24509d0836fdad5e55e5ebcce42850ba64c4
ichha <thapa.ichha@gmail.com>
2025-11-02 15:21:33 +0545

    Update views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,26 +1,4 @@
 from django.views.generic import TemplateView
-from extras.plugins import PluginTemplateExtension
-
 
 class HelloView(TemplateView):
-    template_name = "hello_world/hello.html"
-
-
-# Optional: expose the view as a plugin template extension (not required for a page)
-class HelloWorldExtension(PluginTemplateExtension):
-    model = "dcim.device"
-
-    def right_page(self):
-        return """
-        <div class="panel panel-default">
-            <div class="panel-heading"><strong>Hello World</strong></div>
-            <div class="panel-body">
-                <a href="{% url 'plugins:hello_world:hello' %}" class="btn btn-primary">
-                    Go to Hello page
-                </a>
-            </div>
-        </div>
-        """
-
-# Uncomment the line below if you want the extension to appear on device detail pages
-# template_extensions = [HelloWorldExtension]
+    template_name = 'hello_world/hello.html'
