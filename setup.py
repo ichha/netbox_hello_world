1ee89f3fcb89f56f785608c7a03deffbcd1f8030
ichha <thapa.ichha@gmail.com>
2025-11-02 14:56:54 +0545

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netbox-hello",
+    version="0.1.1",
+    description="NetBox Hello World plugin with navigation (for NetBox 4.4.4)",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
