cee6df6fded785570910e2b0d5d52a6ec7cfabbc
ichha <thapa.ichha@gmail.com>
2025-11-02 15:25:38 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name='netbox-hello',
-    version='0.1',
-    description='Simple demo plugin for NetBox',
-    author='Ichha Kr Thapa',
+    name="netbox-hello-world",
+    version="0.1.0",
+    description="Hello‑World plugin for NetBox 4.4.4",
     packages=find_packages(),
+    install_requires=["netbox==4.4.4"],
     include_package_data=True,
     zip_safe=False,
 )
